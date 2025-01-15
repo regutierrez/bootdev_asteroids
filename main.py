8278f183818bf1ed50694cb742ace16259d86fef
rafael-ae <rafael.gutierrez@aescape.com>
2025-01-14 21:36:34 -0500

    finish ch2-l8

--- main.py
+++ main.py
@@ -20,14 +20,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
-        player.draw(screen=screen)
+        for obj in drawable:
+            obj.draw(screen=screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
