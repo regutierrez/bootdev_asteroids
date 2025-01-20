60da8a44d56f13face8a6bb02e570eb147574cba
rafael-ae <rafael.gutierrez@aescape.com>
2025-01-19 20:57:15 -0500

    finish asteroid class

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,21 @@
 import pygame
+from circleshape import CircleShape
 
-class Asteroid
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            surface=screen,
+            color="brown",
+            radius=self.radius,
+            width=2,
+            center=(self.x, self.y),
+        )
+
+    def update(self, dt):
+        self.velocity += self.velocity * dt
